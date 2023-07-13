0d7ef698d11d2cd9b270cb06aef00f3ad5a2c38d
kindear <kindear@foxmail.com>
2023-07-13 21:06:48 +0800

    初始化

---
+++ src/main/java/org/lboot/onlyoffice/LucyOnlyofficeApplication.java
@@ -0,0 +1,13 @@
+package org.lboot.onlyoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucyOnlyofficeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LucyOnlyofficeApplication.class, args);
+    }
+
+}
