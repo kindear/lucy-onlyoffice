4963c990edc0acba17e67906cb77cb13f44ba715
kindear <kindear@foxmail.com>
2023-08-14 14:31:50 +0800

    update 0.0.2-r3

--- src/main/java/org/lboot/onlyoffice/controller/OnlyOfficeController.java
+++ src/main/java/org/lboot/onlyoffice/controller/OnlyOfficeController.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.lboot.onlyoffice.service.OfficeCtl;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -47,10 +48,23 @@ public class OnlyOfficeController {
     }
 
     @SneakyThrows
+    @RequestMapping("preview/{id}")
+    @ApiOperation(value = "文档预览(存储系统)")
+    public ModelAndView preview(@PathVariable("id") String id){
+        return officeCtl.previewFile(id);
+    }
+
+    @SneakyThrows
     @RequestMapping("edit/remote")
     @ApiOperation(value = "网络文档编辑",notes = "支持各种类型文档")
     public ModelAndView editRemote(@RequestParam("url") String url){
         return officeCtl.editRemoteFile(url);
     }
 
+    @SneakyThrows
+    @RequestMapping("edit/{id}")
+    @ApiOperation(value = "文档编辑(存储系统)",notes = "支持word,excel等")
+    public ModelAndView edit(@PathVariable("id") String id){
+        return officeCtl.editFile(id);
+    }
 }
